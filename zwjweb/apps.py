d09850c2f4f5e3aa39be85e249e46ed9d97d571d
zwj <wyx20021110@gmail.com>
2022-07-30 09:11:58 +0800

    init

---
+++ zwjweb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ZwjwebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'zwjweb'
---
+++ zwjweb/urls/index.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from zwjweb.views.index import index
+
+urlpatterns = [
+    path('',index,name = 'index'),
+
+
+]
